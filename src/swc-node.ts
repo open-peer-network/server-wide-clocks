e6864741e5a763c3e8d3ba944c66076d7d70f3fb
Rob Christian <rm.rf.etc@gmail.com>
2020-02-29 15:15:58 -0800

    Add remaining swc_node functions

--- src/swc-node.ts
+++ src/swc-node.ts
@@ -1,27 +1,26 @@
-export type Bvv = [number, number];
 export type Dot = [string, number];
-export type NodeClock = [string, Bvv];
-export type NodeClocks = NodeClock[];
+export type BaseBitmapPair = [number, number];
+export type BVV = [string, BaseBitmapPair];
 
 // Returns the entry of a BVV associated with a given ID.
 export const get = (
-	[nodeId, bitmap]: Bvv,
-	bvv: NodeClocks,
+	[nodeId, bitmap]: BaseBitmapPair,
+	bvv: BVV[],
 ) => (
 	bvv.find(([id]) => id === `${nodeId},${bitmap}`) || [0, 0]
 );
 
 // Normalizes an entry pair, by removing dots and adding them to the base
 // if they are contiguous with the base.
-export const norm = ([baseCounter, bitmap]: Bvv): Bvv => (
+export const norm = ([baseCounter, bitmap]: BaseBitmapPair): BaseBitmapPair => (
 	bitmap % 2 ? norm([baseCounter + 1, bitmap >> 1]) : [baseCounter, bitmap]
 );
 
 // Normalizes all entries in the BVV, using norm.
-export const normBvv = (bvv: NodeClocks): NodeClocks => (
+export const normBvv = (bvv: BVV[]): BVV[] => (
 	bvv
 	// normalize all entries
-	.map(([key, bvv]): [string, Bvv] => ([key, norm(bvv)]))
+	.map(([key, bvv]): BVV => ([key, norm(bvv)]))
 	// remove `[0,0]` entries
 	.filter(([, bvv]) => bvv.join(',') !== '0,0')
 );
@@ -29,10 +28,10 @@ export const normBvv = (bvv: NodeClocks): NodeClocks => (
 // Returns the dots in the first clock that are missing from the second
 // clock, but only from entries in the list of ids received as argument.
 export const missingDots = (
-	nc1: NodeClocks,
-	nc2: NodeClocks,
+	nc1: BVV[],
+	nc2: BVV[],
 	ids: string[],
-): NodeClocks => {
+): BVV[] => {
 	return nc1.length < 1 ? [] : nc1.reduce((acc, [id, bvv]) => {
 		if (ids.includes(id)) {
 			const idx = nc2.findIndex(([id0]) => id === id0);
@@ -50,8 +49,8 @@ export const missingDots = (
 };
 
 export const subtractDots = (
-	[count1, bitmap1]: Bvv,
-	[count2, bitmap2]: Bvv,
+	[count1, bitmap1]: BaseBitmapPair,
+	[count2, bitmap2]: BaseBitmapPair,
 ) => {
 	const [dots1, dots2] = count1 > count2 ? [
 		seq(count2 + 1, count1).concat(valuesAux(count1, bitmap1, [])),
@@ -73,7 +72,7 @@ export const seq = (start: number = 0, end: number = 1) => {
 };
 
 // Returns the sequence numbers for the dots represented by an entry.
-export const values = ([baseCounter, bitmap]: Bvv) => (
+export const values = ([baseCounter, bitmap]: BaseBitmapPair) => (
 	seq(1, baseCounter).concat(valuesAux(baseCounter, bitmap, []))
 );
 
@@ -95,11 +94,11 @@ const valuesAux = (
 
 // Adds a dot (ID, Counter) to a BVV.
 export const add = (
-	clocks: NodeClocks,
+	clocks: BVV[],
 	[id, counter]: Dot,
 ) => {
 	const initial = addAux([0, 0], counter);
-	const fn = (entry: Bvv) => addAux(entry, counter);
+	const fn = (entry: BaseBitmapPair) => addAux(entry, counter);
 
 	const newClocks = clocks.slice();
 	const idx = newClocks.findIndex(([id0]) => id0 === id);
@@ -113,13 +112,80 @@ export const add = (
 
 // Adds a dot to a BVV entry, returning the normalized entry.
 export const addAux = (
-	[base, bitmap]: Bvv,
+	[base, bitmap]: BaseBitmapPair,
 	count: number,
+) => (
+	(base < count)
+		? norm([base, bitmap | (1 << (count - base - 1))])
+		: norm([base, bitmap])
+);
+
+// Merges all entries from the two BVVs.
+export const merge = (bvvList1: BVV[], bvvList2: BVV[]) => (
+	normBvv(Object.entries(([...bvvList1, ...bvvList2]).reduce((acc, [id, bvv]) => {
+		acc[id] = acc[id] ? joinAux(acc[id], bvv) : bvv;
+		return acc;
+	}, {})))
+);
+
+// Joins entries from BVV2 that are also IDs in BVV1, into BVV1.
+export const join = (bvvList1: BVV[], bvvList2: BVV[]) => {
+	// filter keys from bvvList2 that are not in bvvList1
+	const bvvList1Keys = bvvList1.map(([id]) => id);
+	const bvvList2b = bvvList2.filter(([id]) => bvvList1Keys.includes(id));
+	// merge bvvList1 with filtered bvvList2b
+	return merge(bvvList1, bvvList2b);
+};
+
+// Returns a (normalized) entry that results from the union of dots from
+// two other entries. Auxiliary function used by join/2.
+export const joinAux = (
+	[count1, bitmap1]: BaseBitmapPair,
+	[count2, bitmap2]: BaseBitmapPair,
+): BaseBitmapPair => (
+	(count1 >= count2)
+		? [count1, bitmap1 | (bitmap2 >> (count1 - count2))]
+		: [count2, bitmap2 | (bitmap1 >> (count2 - count1))]
+);
+
+// Takes and returns a BVV where in each entry, the bitmap is reset to zero.
+export const base = (bvv: BVV[]) => (
+	// normalize all entries
+	// remove all non-contiguous counters w.r.t the base
+	normBvv(bvv).map(([baseCounter, [counter]]) => ([baseCounter, [counter, 0]]))
+);
+
+// Takes a BVV at node Id and returns a pair with sequence number for a new
+// event (dot) at node Id and the original BVV with the new dot added; this
+// function makes use of the invariant that the node BVV for node Id knows all
+// events generated at Id, i.e., the first component of the pair denotes the
+// last event, and the second component, the bitmap, is always zero.
+export const event = (bvv: BVV[], id: string) => {
+	// find the next counter for id
+	const match = bvv.find(([id0]) => id0 === id);
+	// since nodes call event with their id, their entry always matches [N, 0]
+	const count = match ? match[1][0] + 1 : 1;
+	// return the new counter and the updated BVV
+	return [count, add(bvv, [id, count]).sort(([id1], [id2]) => Number(id1 > id2))];
+};
+
+// Stores an Id-Entry pair in a BVV; if the id already exists, the 
+// associated entry is replaced by the new one.
+export const storeEntry = (
+	id: string,
+	[base]: BaseBitmapPair,
+	bvv: BVV[],
 ) => {
-	if (base < count) {
-		const newBitmap = bitmap | (1 << (count - base - 1));
-		return norm([base, newBitmap]);
-	} else {
-		return norm([base, bitmap]);
+	if (base === 0) return bvv;
+	const match = bvv.find(([id0]) => id0 === id);
+	if (match) {
+		const [,[base2]] = match;
+		if (base2 >= base) {
+			return bvv;
+		}
+		if (base2 < base) {
+			return bvv.map((entry) => entry[0] === id ? [id, [base, 0]] : entry);
+		}
 	}
+	return bvv.concat([[id, [base, 0]]]);
 };
